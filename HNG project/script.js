b53ad38244b78fc7332d5ef9c18b0e1baafc5b3d
Alexander Lashinde <43205715+Alexylash@users.noreply.github.com>
2023-09-08 22:39:16 +0100

    Add files via upload

--- HNG project/script.js
+++ HNG project/script.js
@@ -1,21 +1,24 @@
-const slackNameElement = document.getElementById("slackUserName");
-        const currentDayElement = document.querySelector("[data-testid=currentDayOfTheWeek]");
-        const currentUTCTimeElement = document.querySelector("[data-testid=currentUTCTime]");
+// script.js
+document.addEventListener('DOMContentLoaded', function () {
+    // Function to get the current day of the week
+    function getCurrentDayOfWeek() {
+        const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        const currentDate = new Date();
+        const dayOfWeek = daysOfWeek[currentDate.getUTCDay()];
+        return dayOfWeek;
+    }
 
-        // Function to get the current day of the week
-        function getCurrentDayOfWeek() {
-            const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-            const currentDate = new Date();
-            const dayOfWeek = daysOfWeek[currentDate.getUTCDay()];
-            return dayOfWeek;
-        }
+    // Function to get the current UTC time in milliseconds
+    function getCurrentUTCTime() {
+        return Date.now();
+    }
 
-        // Function to get the current UTC time in milliseconds
-        function getCurrentUTCTime() {
-            return Date.now();
-        }
+    // Update the elements with dynamic data
+    const slackNameElement = document.querySelector('[data-testid="slackUserName"]');
+    const currentDayElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+    const currentUTCTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
 
-        // Update the elements with dynamic data
-        slackNameElement.textContent = "Your Slack Name"; // Replace with your Slack name
-        currentDayElement.textContent = getCurrentDayOfWeek();
-        currentUTCTimeElement.textContent = `Current UTC Time: ${getCurrentUTCTime()} milliseconds`;
+    slackNameElement.textContent = "Opillo1000"; // Replace with your Slack name
+    currentDayElement.textContent = getCurrentDayOfWeek();
+    currentUTCTimeElement.textContent = `Current UTC Time: ${getCurrentUTCTime()} milliseconds`;
+});
